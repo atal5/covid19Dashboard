4194cef15e8bdd812ffc4146932bc9548dbf6f10
atal5 <atal@uchicago.edu>
2020-03-30 15:19:33 -0500

    Changed md device to 12 on small screen

--- covid19app/app/covid_app.py
+++ covid19app/app/covid_app.py
@@ -3,8 +3,8 @@ from dash.dependencies import Input, Output
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-#from . import covid_conf_analysis as cv
-import covid_conf_analysis as cv
+from . import covid_conf_analysis as cv
+#import covid_conf_analysis as cv
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
 import plotly.express as px
